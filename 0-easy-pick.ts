d60a4b6a673c2d8ad1f0240fa1a19828bd1d2503
Ryszard Matula <rmatula@us.edu.pl>
2022-07-25 19:31:15 +0200

    0-easy-pick.ts

---
+++ 0-easy-pick.ts
@@ -0,0 +1,16 @@
+type MyPick<TObj, TKey extends keyof TObj> = {
+  [SpecificKey in TKey]: TObj[SpecificKey];
+};
+
+type Yeah = MyPick<{ a: number; b: number }, "a">;
+
+// type Yeah = {
+//   a: number;
+// }
+
+type Wow = MyPick<{ a: number; b: number }, "a" | "b">;
+
+// type Wow = {
+//   a: number;
+//   b: number;
+// }
