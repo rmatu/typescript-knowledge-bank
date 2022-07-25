a04c1343d499fbcbfcbeebecd1e996a0dc9e2e0d
Ryszard Matula <rmatula@us.edu.pl>
2022-07-25 20:30:30 +0200

    05-dynamic-function-argument

---
+++ 05-dynamic-function-arguments.ts
@@ -0,0 +1,30 @@
+export type Event =
+  | {
+      type: "LOG_IN";
+      payload: {
+        userId: string;
+      };
+    }
+  | {
+      type: "SIGN_OUT";
+    };
+
+const sendEvent = <Type extends Event["type"]>(
+  ...args: Extract<Event, { type: Type }> extends { payload: infer TPayload }
+    ? [type: Type, payload: TPayload]
+    : [type: Type]
+) => {};
+
+/**
+ * Correct
+ */
+sendEvent("SIGN_OUT");
+sendEvent("LOG_IN", { userId: "123" });
+
+/**
+ * Should error
+ */
+sendEvent("SIGN_OUT", {});
+sendEvent("LOG_IN", { userId: 123 });
+sendEvent("LOG_IN", {});
+sendEvent("LOG_IN", {});
