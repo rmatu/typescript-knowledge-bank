5e7c53cce5d953b00e7cc8c477595917e12642d6
Ryszard Matula <rmatula@us.edu.pl>
2022-07-25 20:14:27 +0200

    3-loose-autocomplete-react

---
+++ 03-loose-autocomplete-react.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+type IconSize2 = "sm" | "xs" | Omit<string, "xs" | "sm">;
+type IconSize = LooseAutocomplete<"sm" | "xs">;
+
+type LooseAutocomplete<T extends string> = T | Omit<string, T>;
+
+interface IconProps {
+  size: IconSize;
+}
+
+export const Icon = (props: IconProps) => {
+  return <></>;
+};
+
+const Comp1: React.FC = () => {
+  return (
+    <>
+      <Icon size="xs" />
+      <Icon size="something" />
+    </>
+  );
+};
